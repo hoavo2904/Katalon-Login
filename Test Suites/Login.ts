8bac55bd49e6435c0666116d2c7716691af5d530
hoavo2904 <53227707+hoavo2904@users.noreply.github.com>
2020-09-25 17:53:07 +0700

    commit 2

---
+++ Test Suites/Login.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Login with valid account</description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0ffcc847-8f68-454b-891a-089d26364cad</testSuiteGuid>
+   <testCaseLink>
+      <guid>8662ef48-ec37-4657-ab55-85c1c1a4facf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case 1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38e12404-f207-434e-bb6b-17f103d134a0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case 2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6b48e8ec-3e3c-49f1-9ca7-dc83c618a268</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case 3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0c88df38-fd3b-4782-aae8-39c882ff54e9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case 4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>99de488e-b8c4-4c63-918c-f0f906fddd89</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Case 5</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
